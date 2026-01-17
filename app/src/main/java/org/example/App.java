7382163c60862a9424c4733db527689bd11a652c
w32145 <w32145850@gmail.com>
2026-01-17 12:24:28 -0600

    Implement ounce conversion program in App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,8 +1,23 @@
 package org.example;
+import java.util.Scanner;
+
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Write a driver program for the Converter class here!");
-    System.out.println("You can delete these print statements and replace them with your code.");
+System.out.println("Welcome to the Ounce Conversion Program!\n");
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("How many ounces do you have? ");
+    int ounces = scanner.nextInt();
+    
+    scanner.close();
+
+
+    Converter converter = new Converter();
+
+    System.out.println(converter.toPounds(ounces));
+    System.out.println(converter.toPoundsAndOunces(ounces));
+
+    System.out.println("\nThank you for using the OCP!");
   }
 }
