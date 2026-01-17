ce44818c263e02162947a47d9effb6a6270b7ce0
w32145 <w32145850@gmail.com>
2026-01-17 12:25:07 -0600

    Add conversion methods for ounces to pounds
    Implemented methods to convert ounces to pounds and pounds with ounces.

--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,14 +1,25 @@
 package org.example;
 
 public class Converter {
-  // Write the code needed to make the tests in
-  // TestConverter.java pass. I have provided the
-  // tests for you in this lab to give you an example
-  // of how they should look. In the future, you will
-  // be responsible for writing your own tests.
 
-  // After you have written the code to make the tests
-  // pass, exercise your code in a driver function in
-  // App.java. This shows that your business logic can
-  // run in a console program.
+  public String toPounds(int ounces) {
+    double pounds = ounces / 16.0;
+
+    if (pounds == 1.0) {
+      return 1.0000 + " lb";
+    }
+    return String.format("%.4f", pounds) + " lbs";
+  }
+
+  public String toPoundsAndOunces(int ounces) {
+    int pounds = ounces / 16;
+    int remainder = ounces % 16;
+
+    if (pounds == 1) {
+      return pounds + " lb " + remainder + " oz";
+    }
+    
+      return pounds + " lbs " + remainder + " oz";
+  }
+  
 }
